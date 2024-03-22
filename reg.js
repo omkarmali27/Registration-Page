36b9a2f1c70183f5605e9aae4a33fd730b0ce6cc
omkarmali27 <144773020+omkarmali27@users.noreply.github.com>
2024-03-22 12:28:47 +0530

    Add files via upload

---
+++ reg.js
@@ -0,0 +1,32 @@
+let un = document.getElementById("email");
+let pass = document.getElementById("password");
+let add = document.getElementById("adderss");
+let gd = document.getElementById("graduation");
+let brnch = document.getElementById("branch");
+let yop = document.getElementById("yop");
+
+function registerUser() {
+	let userData={
+		username:un.value,
+		password:pass.value,
+		address:add.value,
+		graduation:gd.value,
+		branch:brnch.value,
+		yop:yop.value,
+	}
+	saveData(userData);
+	clearFeild();
+	open("Login.html");
+}
+	
+	function clearFeild(){
+		un.value="";
+		pass.value="";
+		add.value="";
+		gd.value="";
+		brnch.value="";
+		yop.value="";
+	}
+function saveData(userData){
+	localStorage.setItem("userInfo",userData);
+}
